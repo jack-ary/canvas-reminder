9ad9248bc54c7c85d8b236de5770824a52297a06
Jack Ary <92411894+jack-ary@users.noreply.github.com>
2022-03-18 12:08:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import time
 notification.notify(title='Canvas Reminder', message='message', app_icon=None, timeout=10)
 # time.sleep(30)
 
-API_URl = "https://canvas.calpoly.edu"
-API_KEY = "15279~16MobaoYyHNBzFrol9P2cIGNV8Md98g7kI43hVbWvGHUPAaGbKkIxW30bPSL9XMk"
+API_URl = "Canvas_URL"
+API_KEY = "Access_Token"
 
 canvas = Canvas(API_URl, API_KEY)
 user = canvas.get_user()
 
-course = canvas.get_course(71875)
+course = canvas.get_course(12345) # Course Code
 
 assignments = course.get_assignments()
 for assignment in assignments:
@@ -22,11 +22,3 @@ courses = canvas.get_courses(enrollment_state='active')
 for course in courses:
     print(course)
 
-# Getting user logins:
-# logins = user.get_user_logins()
-# for login in logins:
-#     print(login)
-
-# users = course.get_users()
-# for user in users:
-#     print(user)
